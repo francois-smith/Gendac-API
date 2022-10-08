5b1d655306e6ce108d0a27172804397b66bbcae1
Francois Smith <steamfaffa@gmail.com>
2022-10-08 12:02:17 +0200

    Added New Controller and Model

---
+++ ProficiencyAssessment/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using ProficiencyAssessment.Models;
+
+namespace ProficiencyAssessment.Controllers
+{
+    [ApiController]
+    [Route("api/ProductsController")]
+    [EnableCors("ReactPolicy")]
+    [Produces("application/json")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetProducts")]
+        public IActionResult Get()
+        {
+            var products = new Product[]
+            {
+                new Product { ProductId = 1, Name = "Tomato Soup", Category = 2, Price = 1 },
+                new Product { ProductId = 2, Name = "Yo-yo", Category = 1, Price = 3.75 },
+                new Product { ProductId = 3, Name = "Hammer", Category = 2, Price = 16.99 }
+            };
+            return Ok(products);
+        }
+    }
+}
---
+++ ProficiencyAssessment/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProficiencyAssessment.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = "";
+        public int Category { get; set; }
+        public double Price { get; set; }
+        public string? Summary { get; set; }
+    }
+}
+
