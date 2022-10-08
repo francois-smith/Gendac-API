46bbb26874962feb3592f68dbc5cbe7991ee6f7a
Francois Smith <steamfaffa@gmail.com>
2022-10-08 12:03:00 +0200

    Added proxy to link Backend and Frontend

---
+++ ProficiencyAssessment/ClientApp/src/setupProxy.js
@@ -0,0 +1,23 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { env } = require('process');
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:37302';
+
+//Allows the app to make requests to the ASP.NET Core API
+//New controllers with route starting with api/ will be reachable without any extra configuration
+const context =  [
+    "/api",
+];
+
+module.exports = function(app) {
+  const appProxy = createProxyMiddleware(context, {
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  });
+
+  app.use(appProxy);
+};
