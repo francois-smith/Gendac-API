a051f9242451dbaac5ed4617ccfe4796ade04433
Francois Smith <steamfaffa@gmail.com>
2022-10-08 12:04:04 +0200

    Removed Variable

--- ProficiencyAssessment/ClientApp/src/index.js
+++ ProficiencyAssessment/ClientApp/src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
